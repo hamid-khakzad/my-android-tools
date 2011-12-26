bf727e2594d05d8bfc4d3caf5756f2d7627a1b48
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2011-12-26 10:30:49 +0000

    添加了取消方法

--- MyAndroidTools/src/cn/emagsoftware/ui/UIThread.java
+++ MyAndroidTools/src/cn/emagsoftware/ui/UIThread.java
@@ -8,11 +8,12 @@ import android.os.Looper;
  * <p>该类的功能实现类似于android.os.AsyncTask类，但AsyncTask类内部采用了线程池实现，线程资源不会被立即释放。该类以快速释放的实现，提供给用户一个第二选择
  * <p>该类支持在非UI-Thread中创建并启动
  * @author Wendell
- * @version 1.7
+ * @version 2.0
  */
 public class UIThread extends Thread {
 	
 	protected Context context = null;
+	protected boolean isCancelled = false;
 	protected Handler handler = new Handler(Looper.getMainLooper());
 	
 	public UIThread(Context context){
@@ -23,12 +24,17 @@ public class UIThread extends Thread {
 	public final void run(){
 		super.run();
 		try{
+			if(isCancelled) return;
 			final boolean[] isOK = new boolean[1];
 			isOK[0] = false;
 			handler.post(new Runnable() {
 				@Override
 				public void run() {
 					// TODO Auto-generated method stub
+					if(isCancelled){
+						isOK[0] = true;
+						return;
+					}
 					onBeginUI(context);
 					isOK[0] = true;
 				}
@@ -36,10 +42,12 @@ public class UIThread extends Thread {
 			while(!isOK[0]){
 				sleep(100);
 			}
+			if(isCancelled) return;
 			final Object result = onRunNoUI(context);
 			handler.post(new Runnable(){
 				@Override
 				public void run() {
+					if(isCancelled) return;
 					onSuccessUI(context,result);
 				}
 			});
@@ -47,17 +55,15 @@ public class UIThread extends Thread {
 			handler.post(new Runnable(){
 				@Override
 				public void run() {
+					if(isCancelled) return;
 					onExceptionUI(context,e);
 				}
 			});
-		}finally{
-			handler.post(new Runnable(){
-				@Override
-				public void run() {
-					onFinallyUI(context);
-				}
-			});
 		}
+	}
+	
+	public void cancel(){
+		isCancelled = true;
 	}
 	
 	public void postProgress(final Object progress){
@@ -79,7 +85,5 @@ public class UIThread extends Thread {
 	protected void onSuccessUI(Context context,Object result){}
 	
 	protected void onExceptionUI(Context context,Exception e){}
-	
-	protected void onFinallyUI(Context context){}
 	
 }
