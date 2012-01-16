bf748a7d2eb3f009a448355f52550419fa143b8c
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2012-01-16 09:30:25 +0000

    创建该类

---
+++ trunk/MyAndroidTools/src/cn/emagsoftware/ui/UIUtilities.java
@@ -0,0 +1,37 @@
+package cn.emagsoftware.ui;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import android.widget.Gallery;
+
+public final class UIUtilities {
+	
+	private UIUtilities(){}
+	
+	/**
+	 * <p>使用深度反射机制，在Gallery上动态滚动指定的距离
+	 * @param gallery
+	 * @param distance
+	 */
+	public static void scrollGallery(Gallery gallery, int distance){
+		try{
+			Field field = Gallery.class.getDeclaredField("mFlingRunnable");
+			field.setAccessible(true);
+			Object flingRunnable = field.get(gallery);
+			Method method = flingRunnable.getClass().getDeclaredMethod("startUsingDistance", int.class);
+			method.setAccessible(true);
+			method.invoke(flingRunnable, distance);
+		}catch(NoSuchFieldException e){
+			throw new RuntimeException(e);
+		}catch(IllegalAccessException e){
+			throw new RuntimeException(e);
+		}catch(NoSuchMethodException e){
+			throw new RuntimeException(e);
+		}catch(InvocationTargetException e){
+			throw new RuntimeException(e);
+		}
+	}
+	
+}
