b8577d30529afecbfd5fc6bca352cb65f2c837a8
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2012-03-05 03:44:36 +0000

    修改了日志的等级控制

--- MyAndroidTools/src/cn/emagsoftware/util/LogManager.java
+++ MyAndroidTools/src/cn/emagsoftware/util/LogManager.java
@@ -6,25 +6,40 @@ import android.util.Log;
  * <p>日志管理类，主要对日志做了以下几点功能补充：<br>
  *    1.强制执行TAG参数使用类名的规范<br>
  *    2.避免了直接输出变量可能导致的空指针异常<br>
- *    3.增加了是否显示日志的开关，日志是否显示可统一控制
+ *    3.增加了显示开关，可根据等级控制日志显示与否
  * @author Wendell
  *
  */
 public final class LogManager {
 	
-	public static boolean ENABLE_LOGGING = true;
+	public static int LOGGING_LEVEL = Log.VERBOSE;
 	
 	private LogManager(){}
 	
+	public static void logV(Class<? extends Object> tag,String msg){
+		if(Log.VERBOSE >= LOGGING_LEVEL){
+			if(msg == null) msg = "";
+			Log.v(tag.getSimpleName(), msg);
+		}
+	}
+	
+	public static void logV(Class<? extends Object> tag,String msg,Throwable tr){
+		if(Log.VERBOSE >= LOGGING_LEVEL){
+			if(msg == null) msg = "";
+			if(tr == null) Log.v(tag.getSimpleName(), msg);
+			else Log.v(tag.getSimpleName(), msg, tr);
+		}
+	}
+	
 	public static void logD(Class<? extends Object> tag,String msg){
-		if(ENABLE_LOGGING){
+		if(Log.DEBUG >= LOGGING_LEVEL){
 			if(msg == null) msg = "";
 			Log.d(tag.getSimpleName(), msg);
 		}
 	}
 	
 	public static void logD(Class<? extends Object> tag,String msg,Throwable tr){
-		if(ENABLE_LOGGING){
+		if(Log.DEBUG >= LOGGING_LEVEL){
 			if(msg == null) msg = "";
 			if(tr == null) Log.d(tag.getSimpleName(), msg);
 			else Log.d(tag.getSimpleName(), msg, tr);
@@ -32,14 +47,14 @@ public final class LogManager {
 	}
 	
 	public static void logI(Class<? extends Object> tag,String msg){
-		if(ENABLE_LOGGING){
+		if(Log.INFO >= LOGGING_LEVEL){
 			if(msg == null) msg = "";
 			Log.i(tag.getSimpleName(), msg);
 		}
 	}
 	
 	public static void logI(Class<? extends Object> tag,String msg,Throwable tr){
-		if(ENABLE_LOGGING){
+		if(Log.INFO >= LOGGING_LEVEL){
 			if(msg == null) msg = "";
 			if(tr == null) Log.i(tag.getSimpleName(), msg);
 			else Log.i(tag.getSimpleName(), msg, tr);
@@ -47,14 +62,21 @@ public final class LogManager {
 	}
 	
 	public static void logW(Class<? extends Object> tag,String msg){
-		if(ENABLE_LOGGING){
+		if(Log.WARN >= LOGGING_LEVEL){
 			if(msg == null) msg = "";
 			Log.w(tag.getSimpleName(), msg);
 		}
 	}
 	
+	public static void logW(Class<? extends Object> tag,Throwable tr){
+		if(Log.WARN >= LOGGING_LEVEL){
+			if(tr == null) Log.w(tag.getSimpleName(), "");
+			else Log.w(tag.getSimpleName(), tr);
+		}
+	}
+	
 	public static void logW(Class<? extends Object> tag,String msg,Throwable tr){
-		if(ENABLE_LOGGING){
+		if(Log.WARN >= LOGGING_LEVEL){
 			if(msg == null) msg = "";
 			if(tr == null) Log.w(tag.getSimpleName(), msg);
 			else Log.w(tag.getSimpleName(), msg, tr);
@@ -62,14 +84,14 @@ public final class LogManager {
 	}
 	
 	public static void logE(Class<? extends Object> tag,String msg){
-		if(ENABLE_LOGGING){
+		if(Log.ERROR >= LOGGING_LEVEL){
 			if(msg == null) msg = "";
 			Log.e(tag.getSimpleName(), msg);
 		}
 	}
 	
 	public static void logE(Class<? extends Object> tag,String msg,Throwable tr){
-		if(ENABLE_LOGGING){
+		if(Log.ERROR >= LOGGING_LEVEL){
 			if(msg == null) msg = "";
 			if(tr == null) Log.e(tag.getSimpleName(), msg);
 			else Log.e(tag.getSimpleName(), msg, tr);
