c7afd3b72aea55ba95b7ff448b5eda66292ff82a
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2012-03-13 09:23:56 +0000

    添加了若干节约bitmap的函数

--- MyAndroidTools/src/cn/emagsoftware/memory/MemoryManager.java
+++ MyAndroidTools/src/cn/emagsoftware/memory/MemoryManager.java
@@ -2,18 +2,81 @@ package cn.emagsoftware.memory;
 
 import java.util.List;
 
-import cn.emagsoftware.util.LogManager;
-
 import android.app.ActivityManager;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.view.ViewGroup;
+import cn.emagsoftware.util.LogManager;
 
 public final class MemoryManager {
 	
 	private MemoryManager(){}
+	
+	/**
+	 * <p>获取指定了尺寸限制的BitmapFactory.Options
+	 * @param options 能获得outWidth和outHeight的BitmapFactory.Options，
+	 *                通常的做法是decode参数inJustDecodeBounds为true的BitmapFactory.Options来获得此参数，此时只包含了Bitmap的尺寸信息，可节约内存
+	 * @param minSideLength 最小的宽度或高度，忽略此项限制可传-1
+	 * @param maxNumOfPixels 最大的宽度*高度，忽略此项限制可传-1
+	 * @return
+	 */
+	public static BitmapFactory.Options getBitmapFactoryOptions(BitmapFactory.Options options, int minSideLength, int maxNumOfPixels){
+		BitmapFactory.Options returnOptions = new BitmapFactory.Options();
+		returnOptions.inDither = false;
+		returnOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;
+		returnOptions.inSampleSize = computeSampleSize(options,minSideLength,maxNumOfPixels);
+		return returnOptions;
+	}
+	
+	/**
+	 * <p>获取BitmapFactory.Options使用到的inSampleSize值
+	 * @param options
+	 * @param minSideLength 最小的宽度或高度，忽略此项限制可传-1
+	 * @param maxNumOfPixels 最大的宽度*高度，忽略此项限制可传-1
+	 * @return
+	 */
+	public static int computeSampleSize(BitmapFactory.Options options, int minSideLength, int maxNumOfPixels) {
+		int initialSize = computeInitialSampleSize(options, minSideLength,
+				maxNumOfPixels);
+
+		int roundedSize;
+		if (initialSize <= 8) {
+			roundedSize = 1;
+			while (roundedSize < initialSize) {
+				roundedSize <<= 1;
+			}
+		} else {
+			roundedSize = (initialSize + 7) / 8 * 8;
+		}
+
+		return roundedSize;
+	}
+	
+	private static int computeInitialSampleSize(BitmapFactory.Options options, int minSideLength, int maxNumOfPixels) {
+		double w = options.outWidth;
+		double h = options.outHeight;
+
+		int lowerBound = (maxNumOfPixels == -1) ? 1 : (int) Math.ceil(Math
+				.sqrt(w * h / maxNumOfPixels));
+		int upperBound = (minSideLength == -1) ? 128 : (int) Math.min(Math
+				.floor(w / minSideLength), Math.floor(h / minSideLength));
+
+		if (upperBound < lowerBound) {
+			// return the larger one when there is no overlapping zone.
+			return lowerBound;
+		}
+
+		if ((maxNumOfPixels == -1) && (minSideLength == -1)) {
+			return 1;
+		} else if (minSideLength == -1) {
+			return lowerBound;
+		} else {
+			return upperBound;
+		}
+	}
 	
 	public static void recycleBitmaps(Drawable drawable){
 		List<Bitmap> bitmaps = AbstractBitmapSelector.drawableToBitmaps(drawable, false);
