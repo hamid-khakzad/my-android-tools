2a83e577cc20a1d73aff52824d477ccb39707dff
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2012-04-12 10:49:04 +0000

    临时提交

--- trunk/MyAndroidTools/src/cn/emagsoftware/ui/adapterview/DataHolder.java
+++ trunk/MyAndroidTools/src/cn/emagsoftware/ui/adapterview/DataHolder.java
@@ -34,15 +34,24 @@ public abstract class DataHolder {
 	/**
 	 * <p>使用当前data更新View时触发，出于节约资源的考虑，View默认会被复用，此时只需要更新View即可
 	 * <p>如果存在异步数据的加载，在更新View时需通过getAsyncData(int index)是否为null来判断所有异步数据是否加载完成，若加载完成，也要更新到View上
-	 * <p>若通过GenericAdapter的构造函数设置了View不复用，也需实现当前方法，因为异步数据执行成功后也会回调此方法，此时updateAsyncDataOnly参数为true
+	 * <p>若通过GenericAdapter的构造函数设置了View不复用，可保持该方法的实现为空
 	 * <p>更新View可以通过ViewHolder来提高效率
 	 * @param context
 	 * @param position
 	 * @param view
 	 * @param data
-	 * @param updateAsyncDataOnly
 	 */
-	public abstract void onUpdateView(Context context,int position,View view,Object data,boolean updateAsyncDataOnly);
+	public abstract void onUpdateView(Context context,int position,View view,Object data);
+	
+	/**
+	 * <p>当前的某一个异步数据被执行成功后触发，若当前的异步数据个数为0，可保持该方法的实现为空
+	 * @param context
+	 * @param position
+	 * @param view
+	 * @param asyncData 执行成功后的异步数据，只能直接使用该值而不能使用getAsyncData(asyncDataIndex)，因为异步数据使用了软引用，该值可以确保对异步数据的绝对引用
+	 * @param asyncDataIndex
+	 */
+	public abstract void onAsyncDataExecuted(Context context,int position,View view,Object asyncData,int asyncDataIndex);
 	
 	/**
 	 * <p>获取构造函数中传入的数据对象
@@ -68,22 +77,12 @@ public abstract class DataHolder {
 	}
 	
 	/**
-	 * <p>设置指定位置的异步数据
+	 * <p>设置指定位置的异步数据，异步数据使用软引用的方式存储
 	 * @param index 异步数据的位置
 	 * @param asyncData
 	 */
 	public void setAsyncData(int index,Object asyncData){
 		if(asyncData instanceof SoftReference<?>) throw new IllegalArgumentException("asyncData can not be a type of SoftReference which is used by itself");
-		mAsyncData[index] = asyncData;
-	}
-	
-	/**
-	 * <p>把指定位置的异步数据调整为弱引用，方便GC进行回收
-	 * @param index
-	 */
-	public void changeAsyncDataToSoftReference(int index){
-		Object asyncData = mAsyncData[index];
-		if(asyncData instanceof SoftReference<?>) return;
 		mAsyncData[index] = new SoftReference<Object>(asyncData);
 	}
 	
