970850e64fa8b0b1a9c870f6bc35274b39b06220
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2012-07-13 08:20:58 +0000

    完成了Fragment的功能模块

---
+++ trunk/MyAndroidTools/src/cn/emagsoftware/ui/fragment/ViewExistsAlwaysFragment.java
@@ -0,0 +1,35 @@
+package cn.emagsoftware.ui.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.View;
+
+public class ViewExistsAlwaysFragment extends Fragment
+{
+    
+    private View mViewPoint = null;
+    
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState)
+    {
+        // TODO Auto-generated method stub
+        super.onViewCreated(view, savedInstanceState);
+        mViewPoint = view;
+    }
+    
+    @Override
+    public final void onDestroyView()
+    {
+        // TODO Auto-generated method stub
+        super.onDestroyView();
+    }
+    
+    @Override
+    public View getView()
+    {
+        // TODO Auto-generated method stub
+        super.getView();
+        return mViewPoint;
+    }
+    
+}
