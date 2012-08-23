b7cfd183afe560a04855942782b976368d5b42d5
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2012-08-23 15:32:03 +0000

    完成了日志系统的改造

--- trunk/MyAndroidTools/src/cn/emagsoftware/util/LogManager.java
+++ trunk/MyAndroidTools/src/cn/emagsoftware/util/LogManager.java
@@ -1,9 +1,10 @@
 package cn.emagsoftware.util;
 
+import cn.emagsoftware.util.jlog.JLog;
 import android.util.Log;
 
 /**
- * <p>日志管理类，主要对日志做了以下几点功能补充：<br> 1.强制执行TAG参数使用类名的规范<br> 2.避免了直接输出变量可能导致的空指针异常<br> 3.增加了输出开关，可根据等级控制日志输出与否
+ * <p>日志管理类，主要对日志做了以下几点功能补充：<br> 1.强制执行TAG参数使用类名的规范<br> 2.避免了直接输出变量可能导致的空指针异常<br> 3.增加了输出开关，可根据等级控制日志输出与否<br> 4.可控制是否同步开启JLog日志
  * 
  * @author Wendell
  * 
@@ -11,10 +12,26 @@ import android.util.Log;
 public final class LogManager
 {
 
+    public static final int VERBOSE       = 2;
+
+    public static final int DEBUG         = 3;
+
+    public static final int INFO          = 4;
+
+    public static final int WARN          = 5;
+
+    public static final int ERROR         = 6;
+
+    public static final int ASSERT        = 7;
+
     /**
-     * <p>通过设置此变量来控制日志的输出，只有等级大于等于设置的等级时日志才会被输出，设置此变量为Log.ASSERT可禁止所有等级的日志输出
+     * <p>通过设置此变量来控制日志的输出，只有等级大于等于设置的等级时日志才会被输出，设置此变量为LogManager.ASSERT可禁止所有等级的日志输出
      */
-    public static int LOGGING_LEVEL = Log.VERBOSE;
+    public static int       LOGGING_LEVEL = VERBOSE;
+    /**
+     * <p>通过设置此变量来控制是否同步开启JLog日志
+     */
+    public static boolean   ENABLED_JLOG  = false;
 
     private LogManager()
     {
@@ -22,127 +39,190 @@ public final class LogManager
 
     public static void logV(Class<? extends Object> tag, String msg)
     {
-        if (Log.VERBOSE >= LOGGING_LEVEL)
+        if (VERBOSE >= LOGGING_LEVEL)
         {
             if (msg == null)
                 msg = "";
-            Log.v(tag.getSimpleName(), msg);
+            String tagStr = tag.getSimpleName();
+            Log.v(tagStr, msg);
+            if (ENABLED_JLOG)
+                JLog.fine(tagStr.concat(":").concat(msg));
         }
     }
 
     public static void logV(Class<? extends Object> tag, String msg, Throwable tr)
     {
-        if (Log.VERBOSE >= LOGGING_LEVEL)
+        if (VERBOSE >= LOGGING_LEVEL)
         {
             if (msg == null)
                 msg = "";
+            String tagStr = tag.getSimpleName();
             if (tr == null)
-                Log.v(tag.getSimpleName(), msg);
-            else
-                Log.v(tag.getSimpleName(), msg, tr);
+            {
+                Log.v(tagStr, msg);
+                if (ENABLED_JLOG)
+                    JLog.fine(tagStr.concat(":").concat(msg));
+            } else
+            {
+                Log.v(tagStr, msg, tr);
+                if (ENABLED_JLOG)
+                    JLog.fine(tagStr.concat(":").concat(msg), tr);
+            }
         }
     }
 
     public static void logD(Class<? extends Object> tag, String msg)
     {
-        if (Log.DEBUG >= LOGGING_LEVEL)
+        if (DEBUG >= LOGGING_LEVEL)
         {
             if (msg == null)
                 msg = "";
-            Log.d(tag.getSimpleName(), msg);
+            String tagStr = tag.getSimpleName();
+            Log.d(tagStr, msg);
+            if (ENABLED_JLOG)
+                JLog.config(tagStr.concat(":").concat(msg));
         }
     }
 
     public static void logD(Class<? extends Object> tag, String msg, Throwable tr)
     {
-        if (Log.DEBUG >= LOGGING_LEVEL)
+        if (DEBUG >= LOGGING_LEVEL)
         {
             if (msg == null)
                 msg = "";
+            String tagStr = tag.getSimpleName();
             if (tr == null)
-                Log.d(tag.getSimpleName(), msg);
-            else
-                Log.d(tag.getSimpleName(), msg, tr);
+            {
+                Log.d(tagStr, msg);
+                if (ENABLED_JLOG)
+                    JLog.config(tagStr.concat(":").concat(msg));
+            } else
+            {
+                Log.d(tagStr, msg, tr);
+                if (ENABLED_JLOG)
+                    JLog.config(tagStr.concat(":").concat(msg), tr);
+            }
         }
     }
 
     public static void logI(Class<? extends Object> tag, String msg)
     {
-        if (Log.INFO >= LOGGING_LEVEL)
+        if (INFO >= LOGGING_LEVEL)
         {
             if (msg == null)
                 msg = "";
-            Log.i(tag.getSimpleName(), msg);
+            String tagStr = tag.getSimpleName();
+            Log.i(tagStr, msg);
+            if (ENABLED_JLOG)
+                JLog.info(tagStr.concat(":").concat(msg));
         }
     }
 
     public static void logI(Class<? extends Object> tag, String msg, Throwable tr)
     {
-        if (Log.INFO >= LOGGING_LEVEL)
+        if (INFO >= LOGGING_LEVEL)
         {
             if (msg == null)
                 msg = "";
+            String tagStr = tag.getSimpleName();
             if (tr == null)
-                Log.i(tag.getSimpleName(), msg);
-            else
-                Log.i(tag.getSimpleName(), msg, tr);
+            {
+                Log.i(tagStr, msg);
+                if (ENABLED_JLOG)
+                    JLog.info(tagStr.concat(":").concat(msg));
+            } else
+            {
+                Log.i(tagStr, msg, tr);
+                if (ENABLED_JLOG)
+                    JLog.info(tagStr.concat(":").concat(msg), tr);
+            }
         }
     }
 
     public static void logW(Class<? extends Object> tag, String msg)
     {
-        if (Log.WARN >= LOGGING_LEVEL)
+        if (WARN >= LOGGING_LEVEL)
         {
             if (msg == null)
                 msg = "";
-            Log.w(tag.getSimpleName(), msg);
+            String tagStr = tag.getSimpleName();
+            Log.w(tagStr, msg);
+            if (ENABLED_JLOG)
+                JLog.warning(tagStr.concat(":").concat(msg));
         }
     }
 
     public static void logW(Class<? extends Object> tag, Throwable tr)
     {
-        if (Log.WARN >= LOGGING_LEVEL)
+        if (WARN >= LOGGING_LEVEL)
         {
+            String tagStr = tag.getSimpleName();
             if (tr == null)
-                Log.w(tag.getSimpleName(), "");
-            else
-                Log.w(tag.getSimpleName(), tr);
+            {
+                Log.w(tagStr, "");
+                if (ENABLED_JLOG)
+                    JLog.warning(tagStr.concat(":").concat(""));
+            } else
+            {
+                Log.w(tagStr, tr);
+                if (ENABLED_JLOG)
+                    JLog.warning(tagStr.concat(":").concat(""), tr);
+            }
         }
     }
 
     public static void logW(Class<? extends Object> tag, String msg, Throwable tr)
     {
-        if (Log.WARN >= LOGGING_LEVEL)
+        if (WARN >= LOGGING_LEVEL)
         {
             if (msg == null)
                 msg = "";
+            String tagStr = tag.getSimpleName();
             if (tr == null)
-                Log.w(tag.getSimpleName(), msg);
-            else
-                Log.w(tag.getSimpleName(), msg, tr);
+            {
+                Log.w(tagStr, msg);
+                if (ENABLED_JLOG)
+                    JLog.warning(tagStr.concat(":").concat(msg));
+            } else
+            {
+                Log.w(tagStr, msg, tr);
+                if (ENABLED_JLOG)
+                    JLog.warning(tagStr.concat(":").concat(msg), tr);
+            }
         }
     }
 
     public static void logE(Class<? extends Object> tag, String msg)
     {
-        if (Log.ERROR >= LOGGING_LEVEL)
+        if (ERROR >= LOGGING_LEVEL)
         {
             if (msg == null)
                 msg = "";
-            Log.e(tag.getSimpleName(), msg);
+            String tagStr = tag.getSimpleName();
+            Log.e(tagStr, msg);
+            if (ENABLED_JLOG)
+                JLog.severe(tagStr.concat(":").concat(msg));
         }
     }
 
     public static void logE(Class<? extends Object> tag, String msg, Throwable tr)
     {
-        if (Log.ERROR >= LOGGING_LEVEL)
+        if (ERROR >= LOGGING_LEVEL)
         {
             if (msg == null)
                 msg = "";
+            String tagStr = tag.getSimpleName();
             if (tr == null)
-                Log.e(tag.getSimpleName(), msg);
-            else
-                Log.e(tag.getSimpleName(), msg, tr);
+            {
+                Log.e(tagStr, msg);
+                if (ENABLED_JLOG)
+                    JLog.severe(tagStr.concat(":").concat(msg));
+            } else
+            {
+                Log.e(tagStr, msg, tr);
+                if (ENABLED_JLOG)
+                    JLog.severe(tagStr.concat(":").concat(msg), tr);
+            }
         }
     }
 
