a426c8544fc9d5a953497b4e964f5e9abf899acc
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2012-09-03 05:13:10 +0000

    修改了排序Bug

--- MyAndroidTools/src/cn/emagsoftware/xmlpull/simpledom/Element.java
+++ MyAndroidTools/src/cn/emagsoftware/xmlpull/simpledom/Element.java
@@ -6,8 +6,6 @@ import java.util.Map;
 
 import org.xmlpull.v1.XmlPullParserException;
 
-import cn.emagsoftware.util.StringUtilities;
-
 /**
  * <p>简单的xml元素类 <p>当前类的一些条件冲突使用XmlPullParserException抛出，是为了方便外部对xml错误的统一捕获而不至于出现遗漏
  * 
@@ -16,10 +14,9 @@ import cn.emagsoftware.util.StringUtilities;
  */
 public class Element
 {
-    private String                     text              = null;
-    private Map<String, Element>       singleTagChildren = null;
-    private Map<String, List<Element>> children          = null;
-    private boolean                    isLeaf            = false;
+    private String                     text     = "";
+    private Map<String, List<Element>> children = new HashMap<String, List<Element>>();
+    private boolean                    isLeaf   = false;
 
     public Element(boolean isLeaf)
     {
@@ -30,7 +27,7 @@ public class Element
     {
         if (!isLeaf)
             throw new XmlPullParserException("only leaf element can get text!");
-        return StringUtilities.toStringWhenNull(text, "");
+        return text;
     }
 
     public void setText(String text) throws XmlPullParserException
@@ -46,21 +43,7 @@ public class Element
     {
         if (isLeaf)
             throw new XmlPullParserException("leaf element can not get children!");
-        if (singleTagChildren != null)
-            return SimpleDomManager.convertDom(singleTagChildren);
-        if (children != null)
-            return children;
-        return new HashMap<String, List<Element>>();
-    }
-
-    public void setSingleTagChildren(Map<String, Element> children) throws XmlPullParserException
-    {
-        if (isLeaf)
-            throw new XmlPullParserException("leaf element can not set children!");
-        if (children == null)
-            throw new NullPointerException();
-        this.singleTagChildren = children;
-        this.children = null;
+        return children;
     }
 
     public void setChildren(Map<String, List<Element>> children) throws XmlPullParserException
@@ -69,7 +52,6 @@ public class Element
             throw new XmlPullParserException("leaf element can not set children!");
         if (children == null)
             throw new NullPointerException();
-        this.singleTagChildren = null;
         this.children = children;
     }
 
