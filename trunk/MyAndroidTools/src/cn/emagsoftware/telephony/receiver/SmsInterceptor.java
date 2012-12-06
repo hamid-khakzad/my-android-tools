17c4aecd39fb27cc219a551fa810e43bba291fb6
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2012-12-06 15:26:28 +0000

    重写该类格式

--- trunk/MyAndroidTools/src/cn/emagsoftware/telephony/receiver/SmsInterceptor.java
+++ trunk/MyAndroidTools/src/cn/emagsoftware/telephony/receiver/SmsInterceptor.java
@@ -3,81 +3,83 @@ package cn.emagsoftware.telephony.receiver;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import cn.emagsoftware.telephony.SmsFilter;
-import cn.emagsoftware.util.LogManager;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Looper;
 import android.telephony.SmsMessage;
+import cn.emagsoftware.telephony.SmsFilter;
+import cn.emagsoftware.util.LogManager;
 
-public abstract class SmsInterceptor extends BroadcastReceiver
+public abstract class SmsInterceptor
 {
 
-    protected Context   context                              = null;
-    protected SmsFilter interceptFilter                      = new SmsFilter()
-                                                             {
-                                                                 @Override
-                                                                 public boolean accept(SmsMessage msg)
-                                                                 {
-                                                                     // TODO Auto-generated method stub
-                                                                     return true;
-                                                                 }
-                                                             };
-    protected Handler   handler                              = new Handler(Looper.getMainLooper());
-    protected boolean   autoUnregisterWhenIntercept          = false;
-    protected int       timeout                              = 0;
-    protected boolean   isDoneForAutoUnregisterWhenIntercept = false;
-    protected boolean   isUnregistered                       = true;
+    private Context           context                              = null;
+    private BroadcastReceiver receiver                             = null;
+    private boolean           autoUnregisterWhenIntercept          = false;
+    private boolean           isDoneForAutoUnregisterWhenIntercept = false;
+    private Handler           handler                              = new Handler();
+    private boolean           isUnregistered                       = true;
+    private boolean           isUnregisteredCompletely             = true;
+    private int               curTimeout                           = -1;
 
-    public SmsInterceptor(Context context, SmsFilter interceptFilter)
+    public SmsInterceptor(Context ctx, SmsFilter interceptFilter)
     {
-        if (context == null)
+        if (ctx == null)
             throw new NullPointerException();
-        this.context = context;
-        if (interceptFilter != null)
-            this.interceptFilter = interceptFilter; // 传null时默认拦截所有
-    }
-
-    @Override
-    public final void onReceive(Context arg0, final Intent arg1)
-    {
-        // TODO Auto-generated method stub
-        if (isUnregistered)
-            return; // 如果已经反注册，将直接返回
-        Bundle bundle = arg1.getExtras();
-        Object[] messages = (Object[]) bundle.get("pdus");
-        final SmsMessage[] smsMessages = new SmsMessage[messages.length];
-        boolean isIntercept = false;
-        for (int i = 0; i < messages.length; i++)
-        {
-            smsMessages[i] = SmsMessage.createFromPdu((byte[]) messages[i]);
-            if (!isIntercept && interceptFilter.accept(smsMessages[i]))
-            {
-                isIntercept = true;
-                this.abortBroadcast();
-            }
-        }
-        if (isIntercept)
-        {
-            // 推迟处理，使abortBroadcast能够生效
-            handler.post(new Runnable()
+        context = ctx;
+        if (interceptFilter == null) // 传null时默认拦截所有
+            interceptFilter = new SmsFilter()
             {
                 @Override
-                public void run()
+                public boolean accept(SmsMessage msg)
                 {
                     // TODO Auto-generated method stub
-                    dealInterceptDelay(arg1, smsMessages);
+                    return true;
                 }
-            });
-        }
+            };
+        final SmsFilter interceptFilterPoint = interceptFilter;
+        receiver = new BroadcastReceiver()
+        {
+            @Override
+            public void onReceive(Context context, final Intent intent)
+            {
+                // TODO Auto-generated method stub
+                if (isUnregistered)
+                    return; // 如果已经反注册，将直接返回
+                Bundle bundle = intent.getExtras();
+                Object[] messages = (Object[]) bundle.get("pdus");
+                final SmsMessage[] smsMessages = new SmsMessage[messages.length];
+                boolean isIntercept = false;
+                for (int i = 0; i < messages.length; i++)
+                {
+                    smsMessages[i] = SmsMessage.createFromPdu((byte[]) messages[i]);
+                    if (!isIntercept && interceptFilterPoint.accept(smsMessages[i]))
+                    {
+                        isIntercept = true;
+                        this.abortBroadcast();
+                    }
+                }
+                if (isIntercept)
+                {
+                    // 推迟处理，使abortBroadcast能够生效
+                    handler.post(new Runnable()
+                    {
+                        @Override
+                        public void run()
+                        {
+                            // TODO Auto-generated method stub
+                            dealInterceptDelay(intent, smsMessages);
+                        }
+                    });
+                }
+            }
+        };
     }
 
-    protected final void dealInterceptDelay(Intent smsIntent, SmsMessage[] smsMessages)
+    private void dealInterceptDelay(Intent smsIntent, SmsMessage[] smsMessages)
     {
         if (isUnregistered)
         {
@@ -106,15 +108,34 @@ public abstract class SmsInterceptor extends BroadcastReceiver
     {
     }
 
-    public void registerMe(int priority)
+    public void setAutoUnregisterWhenIntercept(boolean auto)
     {
+        if (!isUnregisteredCompletely)
+            throw new IllegalStateException("please call this method after it has been unregistered completely.");
+        this.autoUnregisterWhenIntercept = auto;
+    }
+
+    /**
+     * <p>注册并指定优先级和等待超时时间，超时时将回调onTimeout方法并自动反注册 <p>若在超时之前已经反注册，则将不再计算超时
+     * 
+     * @param priority
+     * @param timeout 单位为毫秒，设为0将永不超时
+     */
+    public void registerMe(int priority, int timeout)
+    {
+        if (timeout < 0)
+            throw new IllegalArgumentException("timeout could not be below zero.");
+        if (!isUnregisteredCompletely)
+            throw new IllegalStateException("please call this method after it has been unregistered completely.");
         IntentFilter smsIntentFilter = new IntentFilter();
         smsIntentFilter.setPriority(priority);
         smsIntentFilter.addAction("android.provider.Telephony.SMS_RECEIVED");
         isDoneForAutoUnregisterWhenIntercept = false;
         isUnregistered = false;
-        context.registerReceiver(this, smsIntentFilter);
-        if (timeout > 0)
+        isUnregisteredCompletely = false;
+        context.registerReceiver(receiver, smsIntentFilter, null, handler);
+        curTimeout = timeout;
+        if (curTimeout > 0)
         { // 为0时将永不超时
             new Timer().schedule(new TimerTask()
             {
@@ -128,21 +149,31 @@ public abstract class SmsInterceptor extends BroadcastReceiver
                     if (isDoneForAutoUnregisterWhenIntercept)
                     {
                         cancel();
-                    } else if (timeCount >= timeout)
+                        handler.post(new Runnable()
+                        {
+                            @Override
+                            public void run()
+                            {
+                                // TODO Auto-generated method stub
+                                isUnregisteredCompletely = true;
+                            }
+                        });
+                    } else if (timeCount >= curTimeout)
                     { // 已超时
                         cancel();
-                        if (unregisterMe())
+                        handler.post(new Runnable()
                         {
-                            handler.post(new Runnable()
+                            @Override
+                            public void run()
                             {
-                                @Override
-                                public void run()
+                                // TODO Auto-generated method stub
+                                if (unregisterMe())
                                 {
-                                    // TODO Auto-generated method stub
                                     onTimeout();
                                 }
-                            });
-                        }
+                                isUnregisteredCompletely = true;
+                            }
+                        });
                     }
                 }
             }, 100, 100);
@@ -151,35 +182,21 @@ public abstract class SmsInterceptor extends BroadcastReceiver
 
     public boolean unregisterMe()
     {
-        isDoneForAutoUnregisterWhenIntercept = true; // 在反注册时置为true，使计时器能够尽快退出
+        if (curTimeout > 0)
+            isDoneForAutoUnregisterWhenIntercept = true; // 置该值为true，使超时计时器能够尽快退出
+        else
+            isUnregisteredCompletely = true;
         isUnregistered = true;
         try
         {
-            context.unregisterReceiver(this);
+            context.unregisterReceiver(receiver);
             return true;
         } catch (IllegalArgumentException e)
         {
             // 重复反注册会抛出该异常，如通过代码注册的receiver在当前activity销毁时会自动反注册，若再反注册，即会抛出该异常
-            LogManager.logE(SmsInterceptor.class, "unregister receiver failed.", e);
+            LogManager.logW(SmsInterceptor.class, "unregister receiver failed.", e);
             return false;
         }
-    }
-
-    public void setAutoUnregisterWhenIntercept(boolean auto)
-    {
-        this.autoUnregisterWhenIntercept = auto;
-    }
-
-    /**
-     * <p>设置拦截短信的超时时间，超时时将回调onTimeout方法并自动反注册 <p>若设置了拦截到短信时自动反注册，在拦截到短信时，超时计时器将随之退出而不再计时
-     * 
-     * @param timeout 单位为毫秒，设为0将永不超时
-     */
-    public void setTimeout(int timeout)
-    {
-        if (timeout < 0)
-            throw new IllegalArgumentException("timeout could not be below zero.");
-        this.timeout = timeout;
     }
 
 }
