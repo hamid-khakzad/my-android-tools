63eceda83dae681a21760f2f341fbbfe5c744686
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2013-01-25 08:58:48 +0000

    修改了部分注释

--- trunk/MyAndroidTools/src/cn/emagsoftware/net/http/HttpConnectionManager.java
+++ trunk/MyAndroidTools/src/cn/emagsoftware/net/http/HttpConnectionManager.java
@@ -584,7 +584,7 @@ public final class HttpConnectionManager
 
     /**
      * <p>添加指定的cookie <p>cookie的作用范围由其domain和path决定，无domain时默认为当前url的domain，path是一个相对于domain的路径，无path时默认为当前url的path(直接上级) <p>domain和path形成一个根url，根url具有向下继承性，在该根url下任何子层级的url都继承该cookie
-     * <p>只有相同根url下的同名cookie才会被新的替换，因此不要存在添加一个cookie去替换对应url下能获取到的同名旧cookie的思路
+     * <p>只有相同根url下的同名cookie才会被新的替换，因此指望通过添加cookie能替换对应url下获取到的同名旧cookie时要格外注意
      * 
      * @param url
      * @param cookie
@@ -600,7 +600,7 @@ public final class HttpConnectionManager
 
     /**
      * <p>添加当前响应头中的cookies <p>cookie的作用范围由其domain和path决定，无domain时默认为当前url的domain，path是一个相对于domain的路径，无path时默认为当前url的path(直接上级) <p>domain和path形成一个根url，根url具有向下继承性，在该根url下任何子层级的url都继承该cookie
-     * <p>只有相同根url下的同名cookie才会被新的替换，因此不要存在添加一个cookie去替换对应url下能获取到的同名旧cookie的思路
+     * <p>只有相同根url下的同名cookie才会被新的替换，因此指望通过添加cookie能替换对应url下获取到的同名旧cookie时要格外注意
      * 
      * @param url
      * @param responseHeaders
