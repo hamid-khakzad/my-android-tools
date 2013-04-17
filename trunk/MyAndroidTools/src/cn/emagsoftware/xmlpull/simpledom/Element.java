b39b70a61d2fca96b6bdb7a7a421c4953ca81414
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2013-04-17 04:55:29 +0000

    添加了tag

--- trunk/MyAndroidTools/src/cn/emagsoftware/xmlpull/simpledom/Element.java
+++ trunk/MyAndroidTools/src/cn/emagsoftware/xmlpull/simpledom/Element.java
@@ -12,12 +12,29 @@ import java.util.List;
 public class Element
 {
 
+    private String         tag        = "";
     private String         text       = "";
     private List<String[]> attributes = new ArrayList<String[]>();
     private List<Element>  children   = new ArrayList<Element>();
 
-    public Element()
+    public Element(String tag)
     {
+        if (tag == null)
+            throw new NullPointerException();
+        this.tag = tag;
+    }
+
+    public String getTag()
+    {
+        return tag;
+    }
+
+    public Element setTag(String tag)
+    {
+        if (tag == null)
+            throw new NullPointerException();
+        this.tag = tag;
+        return this;
     }
 
     public String getText()
@@ -44,7 +61,7 @@ public class Element
     }
 
     /**
-     * <p>当前类的一个简单原则是对Children的增、删、改、查操作要求通过getChildren()得到List进行处理 <p>对Children的查找操作（如getChildren(String tagName)）较繁琐，理应提供相关方法，但由于是遍历查找，可能会在上层不清楚具体实现的情况下由于频繁调用导致效率降低，所以交由上层实现
+     * <p>当前类的一个简单原则是对Children的增、删、改、查操作要求通过getChildren()得到List进行处理 <p>对Children的查找操作（如getChildren(String tag)）较繁琐，理应提供相关方法，但由于是遍历查找，可能会在上层不清楚具体实现的情况下由于频繁调用导致效率降低，所以交由上层实现
      * 
      * @return
      */
