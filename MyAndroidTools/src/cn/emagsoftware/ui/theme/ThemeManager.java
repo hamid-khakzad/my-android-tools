891e054548383a8d77107153a90144939beda3e5
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2013-04-25 05:16:18 +0000

    修改了实现机制

--- MyAndroidTools/src/cn/emagsoftware/ui/theme/ThemeManager.java
+++ MyAndroidTools/src/cn/emagsoftware/ui/theme/ThemeManager.java
@@ -1,9 +1,9 @@
 package cn.emagsoftware.ui.theme;
 
 import java.util.List;
-import java.util.Vector;
 
 import cn.emagsoftware.content.pm.PackageMgr;
+import cn.emagsoftware.ui.recreate.RecreateManager;
 
 import android.content.Context;
 import android.content.Intent;
@@ -13,12 +13,11 @@ import android.content.pm.ResolveInfo;
 public final class ThemeManager
 {
 
-    static String                        CUR_PACKAGENAME               = null;
-    static String                        CUR_GENERALTHEME_NAME         = null;
+    static String               CUR_PACKAGENAME               = null;
+    static String               CUR_GENERALTHEME_NAME         = null;
 
-    private static final String          ACTION_MYANDROIDTOOLS_THEME   = "android.intent.action.MYANDROIDTOOLS_THEME";
-    private static final String          CATEGORY_MYANDROIDTOOLS_THEME = "android.intent.category.MYANDROIDTOOLS_THEME";
-    private static Vector<ThemeActivity> mThemeActivities              = new Vector<ThemeActivity>();
+    private static final String ACTION_MYANDROIDTOOLS_THEME   = "android.intent.action.MYANDROIDTOOLS_THEME";
+    private static final String CATEGORY_MYANDROIDTOOLS_THEME = "android.intent.category.MYANDROIDTOOLS_THEME";
 
     private ThemeManager()
     {
@@ -43,19 +42,14 @@ public final class ThemeManager
     /**
      * <p>改变主题
      * 
-     * @param context
      * @param packageName 需要应用的主题包名，如果要使用默认主题，可传null
      * @param generalThemeName 主题包styles.xml中通用主题样式的名字，如果没有通用主题样式，可传null
      */
-    public static void changeTheme(Context context, String packageName, String generalThemeName)
+    public static void changeTheme(String packageName, String generalThemeName)
     {
         CUR_PACKAGENAME = packageName;
         CUR_GENERALTHEME_NAME = generalThemeName;
-        for (int i = 0; i < mThemeActivities.size(); i++)
-        {
-            ThemeActivity tActivity = mThemeActivities.get(i);
-            tActivity.recreate();
-        }
+        RecreateManager.recreateAll();
     }
 
     public static String getCurPackageName()
@@ -76,16 +70,6 @@ public final class ThemeManager
     public static boolean getApplyTheme(Context context)
     {
         return ThemeFactory.createOrUpdateInstance(context, CUR_PACKAGENAME, CUR_GENERALTHEME_NAME).getApplyTheme();
-    }
-
-    public static boolean addThemeActivity(ThemeActivity mThemeActivity)
-    {
-        return mThemeActivities.add(mThemeActivity);
-    }
-
-    public static boolean removeThemeActivity(ThemeActivity mThemeActivity)
-    {
-        return mThemeActivities.remove(mThemeActivity);
     }
 
 }
