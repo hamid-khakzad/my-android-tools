a8bb09f578fdb16380e634f38c829b5349bb8116
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2013-07-09 10:28:10 +0000

    阶段性提交

--- trunk/MyAndroidTools/src/cn/emagsoftware/ui/adapterview/AsyncDataExecutor.java
+++ trunk/MyAndroidTools/src/cn/emagsoftware/ui/adapterview/AsyncDataExecutor.java
@@ -1,263 +1,38 @@
 package cn.emagsoftware.ui.adapterview;
 
-import java.lang.ref.WeakReference;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Set;
-import java.util.concurrent.Executor;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-import android.widget.Adapter;
-import android.widget.AdapterView;
-import android.widget.ExpandableListView;
-import android.widget.ListView;
-import cn.emagsoftware.util.LogManager;
 import cn.emagsoftware.util.OptionalExecutorTask;
 
 public abstract class AsyncDataExecutor
 {
-    private static Executor EXECUTOR = new ThreadPoolExecutor(0,5,30, TimeUnit.SECONDS,new SynchronousQueue<Runnable>(),new ThreadPoolExecutor.CallerRunsPolicy());
+
     private static PushTask                               PUSH_TASK              = new PushTask();
     static
     {
-        PUSH_TASK.execute("");
-    }
-
-    private int                                           mMaxTaskCount          = 2;
-
-    private int                                           mMaxWaitCount          = 20;
-    private WeakReference<AdapterView<? extends Adapter>> mAdapterViewRef        = null;
-    private WeakReference<Object>                 mGenericAdapterRef     = null;
-
-    private LinkedList<DataHolder>                        mPushedHolders         = new LinkedList<DataHolder>();
-    private byte[]                                        mLockExecute           = new byte[0];
-    private Set<OptionalExecutorTask<DataHolder, Object, Object>>    mCurExecuteTasks       = new HashSet<OptionalExecutorTask<DataHolder, Object, Object>>();
-
-    private boolean                                       mNotifyAsyncDataForAll = false;
-
-    public AsyncDataExecutor(int maxTaskCount)
-    {
-        if (maxTaskCount < 1 || maxTaskCount > 5)
-            throw new IllegalArgumentException("maxTaskCount should be between 1 and 5.");
-        this.mMaxTaskCount = maxTaskCount;
-    }
-
-    /**
-     * <p>设置在加载完GenericAdapter的异步数据后是否通知该GenericAdapter对应的所有AdapterView <p>一般不要调用该方法并传true，这会影响到性能，除非当前的GenericAdapter被多个AdapterView共用，且在异步数据加载完后需要即时同步到各个AdapterView
-     * 
-     * @param notifyAsyncDataForAll
-     */
-    public void setNotifyAsyncDataForAll(boolean notifyAsyncDataForAll)
-    {
-        this.mNotifyAsyncDataForAll = notifyAsyncDataForAll;
-    }
-
-    void refreshVariables(AdapterView<? extends Adapter> adapterView, Object genericAdapter)
-    {
-        mMaxWaitCount = adapterView.getLastVisiblePosition() - adapterView.getFirstVisiblePosition() + 2; // AdapterView在第一次布局显示时可能需要加2，所以这里统一加2
-        mAdapterViewRef = new WeakReference<AdapterView<? extends Adapter>>(adapterView);
-        mGenericAdapterRef = new WeakReference<Object>(genericAdapter);
-    }
-
-    void pushAsync(DataHolder dataHolder)
-    {
-        /**
-         * 使用这种实现策略主要有两点:
-         * 1.可减轻主线程压力,使AdapterView操作更流畅
-         * 2.异步数据执行采用了ThreadPoolExecutor.CallerRunsPolicy的线程池策略，该实现可确保不会在主线程堵塞
-         */
-        PUSH_TASK.execPushingAsync(this, dataHolder);
-    }
-
-    private void push(DataHolder dataHolder)
-    {
-        if (dataHolder.mExecuteConfig.mIsExecuting)
-            return;
-        dataHolder.mExecuteConfig.mIsExecuting = true;
-        OptionalExecutorTask<DataHolder, Object, Object> executeTask = null;
-        synchronized (mLockExecute)
-        {
-            if (mCurExecuteTasks.size() < mMaxTaskCount)
-            {
-                executeTask = createExecuteTask();
-                mCurExecuteTasks.add(executeTask);
-            } else
-            {
-                mPushedHolders.addFirst(dataHolder);
-                int excessCount = mPushedHolders.size() - mMaxWaitCount;
-                while (excessCount > 0)
-                {
-                    mPushedHolders.removeLast().mExecuteConfig.mIsExecuting = false;
-                    excessCount--;
-                }
-            }
-        }
-        if (executeTask != null)
-            executeTask.executeOnExecutor(EXECUTOR,dataHolder);
-    }
-
-    private OptionalExecutorTask<DataHolder, Object, Object> createExecuteTask()
-    {
-        return new OptionalExecutorTask<DataHolder, Object, Object>()
-        {
-            private DataHolder curHolder;
-            private boolean    shouldChangeSoftRefByPublish;
-
-            @Override
-            protected Object doInBackground(DataHolder... params)
-            {
-                // TODO Auto-generated method stub
-                while (true)
-                {
-                    if (curHolder == null)
-                    {
-                        curHolder = params[0];
-                        params[0] = null;
-                    } else
-                    {
-                        curHolder.mExecuteConfig.mIsExecuting = false;
-                        if (shouldChangeSoftRefByPublish)
-                        {
-                            // 统一置为软引用可能需要通过publish方式来进行，因为需要保证该操作发生在更新界面publishProgress可能导致的界面重构之后
-                            publishProgress(curHolder);
-                        } else
-                        {
-                            for (int i = 0; i < curHolder.getAsyncDataCount(); i++)
-                            {
-                                curHolder.changeAsyncDataToSoftReference(i);
-                            }
-                        }
-                        synchronized (mLockExecute)
-                        {
-                            curHolder = mPushedHolders.poll();
-                            if (curHolder == null)
-                            {
-                                mCurExecuteTasks.remove(this);
-                                return null;
-                            }
-                        }
-                    }
-                    // 更新界面publishProgress可能导致界面重构，从而重复使用到异步数据，但DataHolder在执行过程中却不允许重复执行，所以执行时要遍历检查所有的异步项，并且先前已执行完的要升级为强引用
-                    shouldChangeSoftRefByPublish = false;
-                    for (int i = 0; i < curHolder.getAsyncDataCount(); i++)
-                    {
-                        Object curAsyncData = curHolder.getAsyncData(i);
-                        if (curAsyncData == null)
-                        {
-                            try
-                            {
-                                Object asyncData = onExecute(curHolder.mExecuteConfig.mPosition, curHolder, i);
-                                if (asyncData == null)
-                                    throw new NullPointerException("the method 'onExecute' returns null");
-                                curHolder.setAsyncData(i, asyncData);
-                                // 更新界面
-                                publishProgress(curHolder, asyncData, i);
-                                shouldChangeSoftRefByPublish = true;
-                            } catch (Exception e)
-                            {
-                                LogManager.logE(AsyncDataExecutor.class, "execute async data failed(position:" + curHolder.mExecuteConfig.mPosition + ",index:" + i + ")", e);
-                            }
-                        } else
-                        {
-                            curHolder.setAsyncData(i, curAsyncData);
-                        }
-                    }
-                }
-            }
-
-            @Override
-            protected void onProgressUpdate(Object... values)
-            {
-                // TODO Auto-generated method stub
-                super.onProgressUpdate(values);
-                if (values.length == 1)
-                {
-                    // 再次publish，这样才能保证统一置为软引用操作发生在更新界面publishProgress可能导致的界面重构之后
-                    publishProgress(values[0], 0);
-                } else if (values.length == 2)
-                {
-                    DataHolder holder = (DataHolder) values[0];
-                    for (int i = 0; i < holder.getAsyncDataCount(); i++)
-                    {
-                        holder.changeAsyncDataToSoftReference(i);
-                    }
-                } else
-                {
-                    Object genericAdapter = mGenericAdapterRef.get();
-                    if (genericAdapter == null)
-                        return;
-                    if (mNotifyAsyncDataForAll)
-                    {
-                        if(genericAdapter instanceof GenericAdapter)
-                            ((GenericAdapter)genericAdapter).notifyDataSetChanged();
-                        else if(genericAdapter instanceof GenericExpandableListAdapter)
-                            ((GenericExpandableListAdapter)genericAdapter).notifyDataSetChanged();
-                    } else
-                    {
-                        AdapterView<? extends Adapter> adapterView = mAdapterViewRef.get();
-                        if (adapterView == null)
-                            return;
-                        DataHolder holder = (DataHolder) values[0];
-                        int position = holder.mExecuteConfig.mPosition;
-                        int wholePosition = -1;
-                        if(genericAdapter instanceof GenericAdapter)
-                        {
-                            GenericAdapter adapter = (GenericAdapter)genericAdapter;
-                            if (position >= adapter.getCount())
-                                return; // DataHolder被执行就意味着Adapter和AdapterView已经同步，此时再判断下Adapter的变化，即可解决所有的不一致问题
-                            if (!holder.equals(adapter.queryDataHolder(position)))
-                                return; // DataHolder被执行就意味着Adapter和AdapterView已经同步，此时再判断下Adapter的变化，即可解决所有的不一致问题
-                            wholePosition = position;
-                            if (adapterView instanceof ListView)
-                                wholePosition = wholePosition + ((ListView) adapterView).getHeaderViewsCount();
-                        }else if(genericAdapter instanceof GenericExpandableListAdapter)
-                        {
-                            GenericExpandableListAdapter adapter = (GenericExpandableListAdapter)genericAdapter;
-                            int groupPos = holder.mExecuteConfig.mGroupPosition;
-                            long packedPosition = -1;
-                            if(groupPos == -1)
-                            {
-                                if (position >= adapter.getGroupCount())
-                                    return; // DataHolder被执行就意味着Adapter和AdapterView已经同步，此时再判断下Adapter的变化，即可解决所有的不一致问题
-                                if (!holder.equals(adapter.queryDataHolder(position)))
-                                    return; // DataHolder被执行就意味着Adapter和AdapterView已经同步，此时再判断下Adapter的变化，即可解决所有的不一致问题
-                                packedPosition = ExpandableListView.getPackedPositionForGroup(position);
-                            }else
-                            {
-                                if(groupPos >= adapter.getGroupCount())
-                                    return; // DataHolder被执行就意味着Adapter和AdapterView已经同步，此时再判断下Adapter的变化，即可解决所有的不一致问题
-                                GroupDataHolder group = adapter.queryDataHolder(groupPos);
-                                if (position >= group.getChildrenCount())
-                                    return; // DataHolder被执行就意味着Adapter和AdapterView已经同步，此时再判断下Adapter的变化，即可解决所有的不一致问题
-                                if (!holder.equals(group.queryChild(position)))
-                                    return; // DataHolder被执行就意味着Adapter和AdapterView已经同步，此时再判断下Adapter的变化，即可解决所有的不一致问题
-                                packedPosition = ExpandableListView.getPackedPositionForChild(groupPos,position);
-                            }
-                            wholePosition = ((ExpandableListView)adapterView).getFlatListPosition(packedPosition);
-                        }
-                        int first = adapterView.getFirstVisiblePosition();
-                        int last = adapterView.getLastVisiblePosition();
-                        if (wholePosition >= first && wholePosition <= last)
-                            holder.onAsyncDataExecuted(adapterView.getContext(), position, adapterView.getChildAt(wholePosition - first), values[1], (Integer) values[2]);
-                    }
-                }
-            }
-        };
+        PUSH_TASK.execute();
     }
 
     /**
      * <p>加载异步数据的回调方法 <p>可抛出任何异常，抛出异常时，外部会认为当前的异步数据执行失败
      * 
-     * @param position 所在AdapterView中的位置
-     * @param dataHolder 用于AdapterView的DataHolder对象
+     * @param position 所在Adapter中的位置
+     * @param dataHolder 用于Adapter的DataHolder对象
      * @param asyncDataIndex 需要加载的DataHolder中异步数据的索引
      * @return 执行后得到的结果
      * @throws Exception
      */
     public abstract Object onExecute(int position, DataHolder dataHolder, int asyncDataIndex) throws Exception;
+
+    /**
+     * <p>如果需要在加载完异步数据后通知使用了当前AdapterView的Adapter的所有AdapterView，则可以覆盖该方法并返回true，但不要轻易这么做，因为会影响到性能</>
+     * @return
+     */
+    public boolean isNotifyAsyncDataForAll()
+    {
+        return false;
+    }
 
     private static class PushTask extends OptionalExecutorTask<Object, Integer, Object>
     {
@@ -282,15 +57,6 @@ public abstract class AsyncDataExecutor
             }
         }
 
-        public void execPushingAsync(final AsyncDataExecutor executor, final DataHolder dataHolder)
-        {
-            queue.offer(new Runnable() {
-                @Override
-                public void run() {
-                    executor.push(dataHolder);
-                }
-            });
-        }
     }
 
 }
--- trunk/MyAndroidTools/src/cn/emagsoftware/ui/adapterview/AsyncDataManager.java
+++ trunk/MyAndroidTools/src/cn/emagsoftware/ui/adapterview/AsyncDataManager.java
@@ -6,13 +6,24 @@ import android.widget.ExpandableListView;
 import android.widget.ListView;
 import android.widget.WrapperListAdapter;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import cn.emagsoftware.util.LogManager;
+import cn.emagsoftware.util.OptionalExecutorTask;
 
 /**
  * Created by Wendell on 13-7-8.
  */
 public final class AsyncDataManager {
+
+    private static Executor EXECUTOR = new ThreadPoolExecutor(0,5,30, TimeUnit.SECONDS,new SynchronousQueue<Runnable>(),new ThreadPoolExecutor.CallerRunsPolicy());
 
     private AsyncDataManager(){}
 
@@ -34,7 +45,7 @@ public final class AsyncDataManager {
             throw new IllegalStateException("please call this method after 'setAdapter(adapter)' is called.");
         int firstPos = view.getFirstVisiblePosition();
         int lastPos = view.getLastVisiblePosition();
-        List<DataHolder> holders = null;
+        List<DataHolder> holders = new ArrayList<DataHolder>();
         if(adapter instanceof GenericAdapter)
         {
             GenericAdapter adapterPoint = (GenericAdapter)adapter;
@@ -53,17 +64,252 @@ public final class AsyncDataManager {
                 holders = adapterPoint.queryDataHolders(firstPos,lastPos);
         }else if(adapter instanceof GenericExpandableListAdapter)
         {
-
+            GenericExpandableListAdapter adapterPoint = (GenericExpandableListAdapter)adapter;
+            ExpandableListView expandableView = (ExpandableListView)view;
+            for(int i = firstPos;i <= lastPos;i++)
+            {
+                long packedPos = expandableView.getExpandableListPosition(i);
+                int groupPos = ExpandableListView.getPackedPositionGroup(packedPos);
+                int childPos = ExpandableListView.getPackedPositionChild(packedPos);
+                if(groupPos == -1)
+                    continue;
+                DataHolder holder = adapterPoint.queryDataHolder(groupPos);
+                if(childPos != -1)
+                    holder = ((GroupDataHolder)holder).queryChild(childPos);
+                holders.add(holder);
+            }
         }else
         {
             throw new IllegalStateException("the adapter for AdapterView can only be GenericAdapter or GenericExpandableListAdapter.");
         }
+        if(holders.size() == 0)
+            return;
 
 
 
 
 
 
+
+
+
+
+    }
+
+    private static class ExecuteRunnable implements Runnable
+    {
+
+        private WeakReference<AdapterView<? extends Adapter>> viewRef = null;
+        private WeakReference<Object> adapterRef = null;
+        private List<DataHolder> holders = null;
+        private AsyncDataExecutor executor = null;
+        private boolean isCancelled = false;
+
+        public ExecuteRunnable(AdapterView<? extends Adapter> view,Object adapter,List<DataHolder> holders,AsyncDataExecutor executor)
+        {
+            this.viewRef = new WeakReference<AdapterView<? extends Adapter>>(view);
+            this.adapterRef = new WeakReference<Object>(adapter);
+            this.holders = holders;
+            this.executor = executor;
+        }
+
+        @Override
+        public void run()
+        {
+            for(DataHolder holder:holders)
+            {
+                if(isCancelled)
+                    return;
+                AdapterView<? extends Adapter> view = viewRef.get();
+                Object adapter = adapterRef.get();
+                if(view == null || adapter == null)
+                    return;
+                if(!holder.mExecuteConfig.mShouldExecute)
+                    continue;
+                if(holder.mExecuteConfig.mIsExecuting)
+                    continue;
+                holder.mExecuteConfig.mIsExecuting = true;
+                ExecuteTask task = new ExecuteTask(view,adapter,holder,executor);
+                view = null;
+                adapter = null;
+                // 采用的ThreadPoolExecutor.CallerRunsPolicy线程池策略可能会堵塞当前线程，故当前操作要在子线程执行，且view和adapter要提前置为null
+                task.executeOnExecutor(EXECUTOR);
+            }
+        }
+
+        public void cancel()
+        {
+            isCancelled = true;
+        }
+
+    }
+
+    private static class ExecuteTask extends OptionalExecutorTask<Object,Object,Object>
+    {
+
+        private WeakReference<AdapterView<? extends Adapter>> viewRef = null;
+        private WeakReference<Object> adapterRef = null;
+        private DataHolder holder = null;
+        private AsyncDataExecutor executor = null;
+
+        public ExecuteTask(AdapterView<? extends Adapter> view,Object adapter,DataHolder holder,AsyncDataExecutor executor)
+        {
+            this.viewRef = new WeakReference<AdapterView<? extends Adapter>>(view);
+            this.adapterRef = new WeakReference<Object>(adapter);
+            this.holder = holder;
+            this.executor = executor;
+        }
+
+        @Override
+        protected Object doInBackground(Object... params) {
+            boolean shouldChangeSoftRefByPublish = false;
+            // 更新界面publishProgress可能导致界面重构，从而重复使用到异步数据，但DataHolder在执行过程中却不允许重复执行，所以执行时要遍历检查所有的异步项，并且先前已执行完的要升级为强引用
+            for (int i = 0; i < holder.getAsyncDataCount(); i++)
+            {
+                Object curAsyncData = holder.getAsyncData(i);
+                if (curAsyncData == null)
+                {
+                    try
+                    {
+                        Object asyncData = executor.onExecute(holder.mExecuteConfig.mPosition, holder, i);
+                        if (asyncData == null)
+                            throw new NullPointerException("the method 'AsyncDataExecutor.onExecute' returns null");
+                        holder.setAsyncData(i, asyncData);
+                        // 更新界面
+                        publishProgress(asyncData, i);
+                        shouldChangeSoftRefByPublish = true;
+                    } catch (Exception e)
+                    {
+                        LogManager.logE(AsyncDataManager.class, "execute async data failed(position:" + holder.mExecuteConfig.mPosition + ",index:" + i + ")", e);
+                    }
+                } else
+                {
+                    holder.setAsyncData(i, curAsyncData);
+                }
+            }
+            holder.mExecuteConfig.mIsExecuting = false;
+            if (shouldChangeSoftRefByPublish)
+            {
+                // 统一置为软引用可能需要通过publish方式来进行，因为需要保证该操作发生在更新界面publishProgress可能导致的界面重构之后
+                publishProgress();
+            } else
+            {
+                for (int i = 0; i < holder.getAsyncDataCount(); i++)
+                {
+                    holder.changeAsyncDataToSoftReference(i);
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Object... values) {
+            super.onProgressUpdate(values);
+            if(values.length == 0)
+            {
+                // 再次publish，这样才能保证统一置为软引用操作发生在更新界面publishProgress可能导致的界面重构之后
+                publishProgress("");
+            }else if(values.length == 1)
+            {
+                for (int i = 0; i < holder.getAsyncDataCount(); i++)
+                {
+                    holder.changeAsyncDataToSoftReference(i);
+                }
+            }else
+            {
+                Object adapterObj = adapterRef.get();
+                if (adapterObj == null)
+                    return;
+                if (executor.isNotifyAsyncDataForAll())
+                {
+                    if(adapterObj instanceof GenericAdapter)
+                        ((GenericAdapter)adapterObj).notifyDataSetChanged();
+                    else if(adapterObj instanceof GenericExpandableListAdapter)
+                        ((GenericExpandableListAdapter)adapterObj).notifyDataSetChanged();
+                } else
+                {
+                    AdapterView<? extends Adapter> adapterView = viewRef.get();
+                    if (adapterView == null)
+                        return;
+                    int position = holder.mExecuteConfig.mPosition;
+                    int wholePosition = -1;
+                    if(adapterObj instanceof GenericAdapter)
+                    {
+                        GenericAdapter adapter = (GenericAdapter)adapterObj;
+                        if (position >= adapter.getCount())
+                            return; // DataHolder被执行就意味着Adapter和AdapterView已经同步，此时再判断下Adapter的变化，即可解决所有的不一致问题
+                        if (!holder.equals(adapter.queryDataHolder(position)))
+                            return; // DataHolder被执行就意味着Adapter和AdapterView已经同步，此时再判断下Adapter的变化，即可解决所有的不一致问题
+                        wholePosition = position;
+                        if (adapterView instanceof ListView)
+                            wholePosition = wholePosition + ((ListView) adapterView).getHeaderViewsCount();
+                    }else if(adapterObj instanceof GenericExpandableListAdapter)
+                    {
+                        GenericExpandableListAdapter adapter = (GenericExpandableListAdapter)adapterObj;
+                        int groupPos = holder.mExecuteConfig.mGroupPosition;
+                        long packedPosition = -1;
+                        if(groupPos == -1)
+                        {
+                            if (position >= adapter.getGroupCount())
+                                return; // DataHolder被执行就意味着Adapter和AdapterView已经同步，此时再判断下Adapter的变化，即可解决所有的不一致问题
+                            if (!holder.equals(adapter.queryDataHolder(position)))
+                                return; // DataHolder被执行就意味着Adapter和AdapterView已经同步，此时再判断下Adapter的变化，即可解决所有的不一致问题
+                            packedPosition = ExpandableListView.getPackedPositionForGroup(position);
+                        }else
+                        {
+                            if(groupPos >= adapter.getGroupCount())
+                                return; // DataHolder被执行就意味着Adapter和AdapterView已经同步，此时再判断下Adapter的变化，即可解决所有的不一致问题
+                            GroupDataHolder group = adapter.queryDataHolder(groupPos);
+                            if (position >= group.getChildrenCount())
+                                return; // DataHolder被执行就意味着Adapter和AdapterView已经同步，此时再判断下Adapter的变化，即可解决所有的不一致问题
+                            if (!holder.equals(group.queryChild(position)))
+                                return; // DataHolder被执行就意味着Adapter和AdapterView已经同步，此时再判断下Adapter的变化，即可解决所有的不一致问题
+                            packedPosition = ExpandableListView.getPackedPositionForChild(groupPos,position);
+                        }
+                        wholePosition = ((ExpandableListView)adapterView).getFlatListPosition(packedPosition);
+                    }
+                    int first = adapterView.getFirstVisiblePosition();
+                    int last = adapterView.getLastVisiblePosition();
+                    if (wholePosition >= first && wholePosition <= last)
+                        holder.onAsyncDataExecuted(adapterView.getContext(), position, adapterView.getChildAt(wholePosition - first), values[0], (Integer) values[1]);
+                }
+            }
+        }
+
+    }
+
+    private static class PushTask extends OptionalExecutorTask<Object,Object,Object>
+    {
+
+        private LinkedList<ExecuteRunnable> runnables = new LinkedList<ExecuteRunnable>();
+
+        @Override
+        protected Object doInBackground(Object... params) {
+            while(true)
+            {
+                ExecuteRunnable curRunnable = null;
+                synchronized (runnables)
+                {
+                    if(runnables.size() > 0)
+                        curRunnable = runnables.removeFirst();
+                }
+                if(curRunnable == null)
+                {
+
+                }else
+                {
+                    curRunnable.run();
+                }
+            }
+        }
+
+        public void push(ExecuteRunnable runnable)
+        {
+            synchronized (runnables)
+            {
+                runnables.addFirst(runnable);
+            }
+        }
 
     }
 
