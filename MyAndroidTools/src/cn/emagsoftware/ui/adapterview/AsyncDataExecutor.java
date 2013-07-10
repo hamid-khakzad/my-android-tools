7d126a55ce35290c2467bf4d20a51ef91b346f9e
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2013-07-10 02:22:06 +0000

    完成了改版

--- MyAndroidTools/src/cn/emagsoftware/ui/adapterview/AsyncDataExecutor.java
+++ MyAndroidTools/src/cn/emagsoftware/ui/adapterview/AsyncDataExecutor.java
@@ -1,18 +1,7 @@
 package cn.emagsoftware.ui.adapterview;
 
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
-
-import cn.emagsoftware.util.OptionalExecutorTask;
-
 public abstract class AsyncDataExecutor
 {
-
-    private static PushTask                               PUSH_TASK              = new PushTask();
-    static
-    {
-        PUSH_TASK.execute();
-    }
 
     /**
      * <p>加载异步数据的回调方法 <p>可抛出任何异常，抛出异常时，外部会认为当前的异步数据执行失败
@@ -32,31 +21,6 @@ public abstract class AsyncDataExecutor
     public boolean isNotifyAsyncDataForAll()
     {
         return false;
-    }
-
-    private static class PushTask extends OptionalExecutorTask<Object, Integer, Object>
-    {
-        private LinkedBlockingQueue<Runnable> queue = new LinkedBlockingQueue<Runnable>();
-
-        @Override
-        protected Object doInBackground(Object... params)
-        {
-            while(true)
-            {
-                try
-                {
-                    /**
-                     * 异步数据线程可以在普通线程中初始化和启动的原因是：
-                     * 1.与异步数据线程同样使用OptionalExecutorTask的PushTask此时已经在主线程初始化了静态的Handler
-                     * 2.异步数据线程不存在onPreExecute()的逻辑，避免了在普通线程中执行可能导致的错误
-                     */
-                    queue.poll(Long.MAX_VALUE,TimeUnit.SECONDS).run();
-                }catch(InterruptedException e)
-                {
-                }
-            }
-        }
-
     }
 
 }
