b30ca26c62e11ceee28ddee0dde64cc3d5d89058
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2013-07-31 06:55:01 +0000

    完成了新功能

---
+++ trunk/MyAndroidTools/src/cn/emagsoftware/net/wifi/direct/CloseApCallback.java
@@ -0,0 +1,13 @@
+package cn.emagsoftware.net.wifi.direct;
+
+/**
+ * Created by Wendell on 13-7-30.
+ */
+public interface CloseApCallback
+{
+
+    public void onClosed();
+
+    public void onError(Exception e);
+
+}
