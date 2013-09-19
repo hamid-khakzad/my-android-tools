9d374909167fae425a9cc3261e1ded91ce633cac
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2013-09-19 14:42:12 +0000

    修改了一个极端情况下可能发生的错误

--- MyAndroidTools/src/cn/emagsoftware/ui/GenericFragmentActivity.java
+++ MyAndroidTools/src/cn/emagsoftware/ui/GenericFragmentActivity.java
@@ -10,12 +10,12 @@ public abstract class GenericFragmentActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         if(!GenericActivity.IS_RUNNING)
         {
             onRestoreStaticState();
             GenericActivity.IS_RUNNING = true;
         }
+        super.onCreate(savedInstanceState); // 可能会触发一些用户行为，故放在恢复静态状态之后
     }
 
     protected abstract void onRestoreStaticState();
