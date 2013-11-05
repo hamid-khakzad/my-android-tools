39210738eb248910467f158383635256ab6ffda9
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2013-11-05 10:28:47 +0000

    添加了simpleBean的操作功能

--- trunk/MyAndroidTools/src/cn/emagsoftware/xmlpull/simpledom/SimpleDomManager.java
+++ trunk/MyAndroidTools/src/cn/emagsoftware/xmlpull/simpledom/SimpleDomManager.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.xmlpull.v1.XmlPullParser;
@@ -139,6 +140,16 @@ public final class SimpleDomManager
             }
             eventType = parser.next();
         }
+    }
+
+    public static HashMap<String,Element> toSimpleBean(List<Element> elements)
+    {
+        HashMap<String,Element> returnVal = new HashMap<String, Element>();
+        for(Element element:elements)
+        {
+            returnVal.put(element.getTag(),element);
+        }
+        return returnVal;
     }
 
 }
