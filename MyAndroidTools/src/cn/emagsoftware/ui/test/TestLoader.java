d5991dc582b889a53f3cc0fd696609d9c16335d4
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2014-08-22 15:15:15 +0000

    修改了测试例子

---
+++ MyAndroidTools/src/cn/emagsoftware/ui/test/TestLoader.java
@@ -0,0 +1,48 @@
+package cn.emagsoftware.ui.test;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.emagsoftware.ui.adapterview.DataHolder;
+import cn.emagsoftware.ui.adapterview.DataHolderLoader;
+
+/**
+ * Created by Wendell on 14-8-22.
+ */
+public class TestLoader extends DataHolderLoader {
+
+    public TestLoader(Context context,List<DataHolder> oldData) {
+        super(context,oldData);
+    }
+
+    @Override
+    public List<DataHolder> loadInBackgroundImpl(Object param) throws Exception {
+        Thread.sleep(8000);
+        int count = 20;
+        List<DataHolder> holders = new ArrayList<DataHolder>(count);
+        for(int i = 0;i < count;i++) {
+            holders.add(new DataHolder(i) {
+                @Override
+                public View onCreateView(Context context, int position, Object data) {
+                    TextView text = new TextView(context);
+                    text.setLayoutParams(new AbsListView.LayoutParams(AbsListView.LayoutParams.FILL_PARENT,140));
+                    text.setGravity(Gravity.CENTER_VERTICAL);
+                    text.setText("POSITION:" + position);
+                    return text;
+                }
+                @Override
+                public void onUpdateView(Context context, int position, View view, Object data) {
+                    ((TextView)view).setText("POSITION:" + position);
+                }
+            });
+        }
+        return holders;
+    }
+
+}
