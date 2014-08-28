8765b495e0abcd6b9fe8c2fbfc03df280de3feb6
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2014-08-28 18:01:07 +0000

    修改了继承

--- MyAndroidTools/src/cn/emagsoftware/ui/test/TestLoader.java
+++ MyAndroidTools/src/cn/emagsoftware/ui/test/TestLoader.java
@@ -9,13 +9,13 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+import cn.emagsoftware.ui.adapterview.BaseLoader;
 import cn.emagsoftware.ui.adapterview.DataHolder;
-import cn.emagsoftware.ui.adapterview.GenericLoader;
 
 /**
  * Created by Wendell on 14-8-22.
  */
-public class TestLoader extends GenericLoader {
+public class TestLoader extends BaseLoader {
 
     public TestLoader(Context context,List<DataHolder> oldData) {
         super(context,oldData);
