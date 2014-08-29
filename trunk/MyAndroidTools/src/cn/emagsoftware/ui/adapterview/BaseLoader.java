c67531289fcf5d053c7276de48846c1bad218e9e
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2014-08-29 14:45:33 +0000

    修改了结构

--- trunk/MyAndroidTools/src/cn/emagsoftware/ui/adapterview/BaseLoader.java
+++ trunk/MyAndroidTools/src/cn/emagsoftware/ui/adapterview/BaseLoader.java
@@ -11,12 +11,8 @@ import cn.emagsoftware.ui.BaseTaskLoader;
  */
 public abstract class BaseLoader extends BaseTaskLoader<List<DataHolder>> {
 
-    public BaseLoader(Context context, List<DataHolder> oldData) {
-        super(context,oldData);
-    }
-
-    @Override
-    public void registerContentObserver(List<DataHolder> data, ForceLoadContentObserver observer) {
+    public BaseLoader(Context context) {
+        super(context);
     }
 
     /**
@@ -24,17 +20,11 @@ public abstract class BaseLoader extends BaseTaskLoader<List<DataHolder>> {
      * @param data
      */
     @Override
-    public final void onReleaseData(List<DataHolder> data) {
+    protected final void onReleaseData(List<DataHolder> data) {
     }
 
-    /**
-     * <p>使用final的原因在于onReleaseData没有任何操作且被标识为final，故不需要进行Clone</>
-     * @param oldData
-     * @return
-     */
     @Override
-    public final List<DataHolder> cloneInBackground(List<DataHolder> oldData) {
-        return oldData;
+    protected void registerContentObserver(List<DataHolder> data, ForceLoadContentObserver observer) {
     }
 
 }
