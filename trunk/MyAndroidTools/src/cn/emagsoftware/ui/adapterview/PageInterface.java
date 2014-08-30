8472ffc6d113bfee521570558eecc54e99088af6
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2014-08-30 18:43:36 +0000

    调整了结构

--- trunk/MyAndroidTools/src/cn/emagsoftware/ui/adapterview/PageInterface.java
+++ trunk/MyAndroidTools/src/cn/emagsoftware/ui/adapterview/PageInterface.java
@@ -6,14 +6,16 @@ package cn.emagsoftware.ui.adapterview;
 public interface PageInterface<D> {
 
     public void forcePageLoad();
+
     /**
      * <p>只返回单页数据，但对Cursor而言，尽管加载的可以是单页数据，但返回的需要是整个结果集</>
+     * @param isRefresh
      * @param start 起始位置，最小为0
      * @param page 起始页，最小为1
      * @return
      * @throws Exception
      */
-    public D loadPageInBackground(int start,int page) throws Exception;
+    public D loadPageInBackground(boolean isRefresh,int start,int page) throws Exception;
     public void setPageCount(int pageCount);
     public boolean isLoadedAll();
     public boolean isException();
