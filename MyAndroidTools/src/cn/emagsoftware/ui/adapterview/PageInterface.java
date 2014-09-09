96b800dd7619ba3262de19829a5b3b10da4a84da
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2014-09-09 02:31:28 +0000

    添加了方法

--- MyAndroidTools/src/cn/emagsoftware/ui/adapterview/PageInterface.java
+++ MyAndroidTools/src/cn/emagsoftware/ui/adapterview/PageInterface.java
@@ -6,7 +6,12 @@ package cn.emagsoftware.ui.adapterview;
 public interface PageInterface<D> {
 
     public void forcePageLoad();
-
+    /**
+     *<p>加载当前已经存在的个数，返回-1表示使用当前缓存的个数，此时loadPageInBackground只需返回下一页的数据；若返回其他值，表示返回了刷新的个数，则loadPageInBackground需要返回所有的数据</>
+     * @return
+     * @throws Exception
+     */
+    public int loadCountInBackground() throws Exception;
     /**
      * <p>只返回单页数据，但对Cursor而言，尽管加载的可以是单页数据，但返回的需要是整个结果集</>
      * @param isRefresh
