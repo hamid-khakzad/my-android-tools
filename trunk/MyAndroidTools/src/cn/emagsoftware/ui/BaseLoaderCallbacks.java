8c8b055054b66e093245d6b76d4385196cf4f741
wendell001.zhang@gmail.com <wendell001.zhang@gmail.com@1255cacf-d987-d3bc-da40-85c6ea0256eb>
2014-09-09 08:34:31 +0000

    添加了新类

---
+++ trunk/MyAndroidTools/src/cn/emagsoftware/ui/BaseLoaderCallbacks.java
@@ -0,0 +1,24 @@
+package cn.emagsoftware.ui;
+
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+
+/**
+ * Created by Wendell on 14-9-9.
+ */
+public abstract class BaseLoaderCallbacks<D> implements LoaderManager.LoaderCallbacks<LoaderResult<D>> {
+
+    @Override
+    public final void onLoadFinished(Loader<LoaderResult<D>> loaderResultLoader, LoaderResult<D> dLoaderResult) {
+        if(dLoaderResult == null) {
+            onLoadFinished(loaderResultLoader,null,null,true);
+        }else {
+            boolean isNew = dLoaderResult.mIsNew;
+            dLoaderResult.mIsNew = false;
+            onLoadFinished(loaderResultLoader,dLoaderResult.getData(),dLoaderResult.getException(),isNew);
+        }
+    }
+
+    protected abstract void onLoadFinished(Loader<LoaderResult<D>> loader,D result,Exception e,boolean isNew);
+
+}
